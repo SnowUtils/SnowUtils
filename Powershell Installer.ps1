3257dc972de35917b81203c5221b9603e7f54c80
TamSchnow265 <TamSchnow265@proton.me>
2022-11-25 21:04:08 +0100

    rbh

---
+++ Powershell Installer.ps1
@@ -0,0 +1,50 @@
+﻿cd $userprofile
+cd Documents
+
+Write-Host 
+"
+ ____                      _   _ _   _ _
+/ ___| _ __   _____      _| | | | |_(_) |___
+\___ \| '_ \ / _ \ \ /\ / / | | | __| | / __|
+ ___) | | | | (_) \ V  V /| |_| | |_| | \__ \
+|____/|_| |_|\___/ \_/\_/  \___/ \__|_|_|___/
+
+
+PowerShell Launcher
+"
+
+Write-Host "Firt time running this? Type " -NoNewline
+Write-Host "init " -ForegroundColor Green -NoNewline
+Write-Host "!"
+
+$ingate = Read-Host -Prompt "Please enter a command"
+
+if ($ingate -eq "init") 
+{
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/SnowUtils/SnowUtils/main/uninstall.ps1" -OutFile "./uninstall.ps1"
+    mkdir sutils
+    Write-Host "sutils initialised! Run this script agan and type install!"
+}
+
+if ($ingate -eq "install") 
+{
+    Write-Host "Downloading SNOWUtils in folder sutils..."
+    cd sutils
+
+    # MathUtils
+    Start-Sleep -Seconds 3
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/SnowUtils/MathUtils/main/graphcalc.py" -OutFile "./graphcalc.py"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/SnowUtils/MathUtils/main/rechteck.py" -OutFile "./rechteck.py"
+
+
+
+
+    cd ..
+}
+
+if ($ingate -eq "uninstall") 
+{
+    ./uninstall.ps1
+    Remove-Item -Path ".\uninstall.ps1"
+    Write-Host "Utils removed, when no error showed up in the console!"
+}
